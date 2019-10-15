1cbcd98eb490173a5734cb4060c597c139e2ab4d
songyakusa <songyakusa@gmail.com>
2019-10-15 23:17:21 +0700

    10

--- app/src/test/java/Main.kt
+++ app/src/test/java/Main.kt
@@ -2,17 +2,30 @@ import com.example.a4totorials.Player
 
 fun main(args: Array<String>){
     val tim =Player("Tim")
-   // println(tim.name)
-   // println(tim.lives)
-   // println(tim.level)
-    //println(tim.score)
     tim.show()
 
     val louise = Player("Louise",5)
     louise.show()
 
-    val gr8 = Player("Louise",5)
+    val gr8 = Player("gr8",5,8)
     val one2watch = Player("Ace",2,5,1000)
     gr8.show()
     one2watch.show()
+    println(one2watch.weapon.name.toUpperCase())
+    println(one2watch.weapon.damageInflicted)
+
+    val axe = Weapon("Axe",12)
+    gr8.weapon =axe
+    println(gr8.weapon.name)
+    println(axe.name)
+
+    axe.damageInflicted = 24
+    println(axe.damageInflicted)
+    println(gr8.weapon.damageInflicted)
+
+    tim.weapon = Weapon("Sword" , 10)
+    println(tim.weapon.name)
+
+    tim.weapon = Weapon("Spear",14)
+    println(tim.weapon.name)
 }
---
+++ app/src/test/java/Weapon.kt
@@ -0,0 +1,2 @@
+class Weapon (val name:String, var damageInflicted:Int){
+}
