e2a42471b3e1a08d834b6f801437fcbc9b516fe0
songyakusa <songyakusa@gmail.com>
2019-10-15 22:34:01 +0700

    7.

--- app/src/test/java/Main.kt
+++ app/src/test/java/Main.kt
@@ -2,12 +2,13 @@ fun main(args: Array<String>){
     println("Hello World")
     println("My first Kotlin program")
 
+    val jim = "James T Kirk"
     var tim: String
     tim = "Tim Buchalka"
     println(tim)
 
-    var timsWeekSalary: Int = 32
-    var timsMonthlySalary: Int =timsWeekSalary * 4
+    val timsWeekSalary: Int = 32
+    val timsMonthlySalary: Int =timsWeekSalary * 4
     println("Tim's $ weekly salary is $$timsWeekSalary")
     println("Monthly, that comes to $timsMonthlySalary")
 
@@ -15,7 +16,7 @@ fun main(args: Array<String>){
 
     val apples : Int = 6
     val oranges :Int = 5
-    var fruit:Int = apples - oranges
+    val fruit = apples - oranges
     println("$apples apples - $oranges oranges leaves $fruit piece(s) of fruit")
 
     println("A quarter of the apples is ${apples / 4}")
@@ -26,5 +27,5 @@ fun main(args: Array<String>){
     println("$weeks weeks is $years years")
 
     println("My name is $tim")
-    println("I can print \$tim")
+    println("jim is $jim")
 }
