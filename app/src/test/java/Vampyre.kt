20090599d16f1804e3f1fc0b71579d7cff856cc2
songyakusa <songyakusa@gmail.com>
2019-10-16 00:33:40 +0700

    21.

--- app/src/test/java/Vampyre.kt
+++ app/src/test/java/Vampyre.kt
@@ -1,4 +1,4 @@
-class Vampyre (name:String):Enemy(name,20,3){
+open class Vampyre (name:String):Enemy(name,20,3){
     override fun  takeDamage(damage:Int){
         super.takeDamage(damage  / 2)
     }
---
+++ app/src/test/java/VampyreKing.kt
@@ -0,0 +1,9 @@
+class VampyreKing (name: String): Vampyre(name){
+    init{
+        hitPoints = 140
+    }
+
+    override fun takeDamage(damage: Int) {
+        super.takeDamage(damage / 2)
+    }
+}
